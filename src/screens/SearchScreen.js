19ebf73792f967b327f5f676767383716383fff0
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2021-04-13 16:20:15 -0400

    carter's food beginnings

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import SearchBar from '../components/SearchBar'
+
+const SearchScreen = () => {
+    return <View>
+        <SearchBar />
+        <Text>Where does my bubbas want to eat?</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+
+})
+export default SearchScreen
